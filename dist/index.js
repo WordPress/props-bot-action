0ffcd8efc7e189f60ce4e9f841a609468481b68b
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2024-02-06 13:24:53 -0500

    Rebuild dist folder.

--- dist/index.js
+++ dist/index.js
@@ -35290,7 +35290,7 @@ class GitHub {
 		const token = core.getInput("token") || process.env.GITHUB_TOKEN || "";
 		this.octokit = github.getOctokit(token);
 
-		const formats = core.getInput("format").split(',').map(function(value){
+		const formats = core.getInput("format").replace(/ /g, '').split(',').map(function(value){
 			return value.trim();
 		}) || "git";
 
