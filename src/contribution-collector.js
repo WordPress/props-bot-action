6481e171cd2a1f247089ec33b2522e751ceeee67
Jonathan Desrosiers <359867+desrosj@users.noreply.github.com>
2025-09-02 09:55:14 -0400

    Prevent failures when `ghost` accounts are encountered (#199)
    * Guard against `null` authors of contributions
    
    When a user deletes their account on GitHub, their activity is no longer associated with any user account and `null` is returned for the `author` field.
    
    This guards against these scenarios to prevent workflow failures.
    
    * Include note in PR comment when ghosts are found
    
    ---------
    
    Co-authored-by: desrosj <desrosj@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: im3dabasia1 <im3dabasia1@git.wordpress.org>

--- src/contribution-collector.js
+++ src/contribution-collector.js
@@ -72,8 +72,17 @@ export async function getContributorsList() {
 	core.debug( 'Raw contributor data:' );
 	core.debug( contributorData );
 
+	// Keep track of whether the Ghostbusters are needed.
+	let hasGhostActivity = false;
+
 	// Process pull request commits.
 	for ( const commit of contributorData?.commits?.nodes || [] ) {
+		// Set a trap for some ghosts.
+		if ( ! commit.commit.author ) {
+			hasGhostActivity = true;
+			continue;
+		}
+
 		/*
 		 * Commits are sometimes made by an email that is not associated with a GitHub account.
 		 * For these, info that may help us guess later.
@@ -100,7 +109,13 @@ export async function getContributorsList() {
 
 	// Process pull request reviews.
 	contributorData.reviews.nodes
-		.filter( ( review ) => ! skipUser( review.author.login ) )
+		.filter( ( review ) => {
+			if ( ! review.author ) {
+				hasGhostActivity = true;
+				return false;
+			}
+			return ! skipUser( review.author.login );
+		} )
 		.forEach( ( review ) =>
 			contributors.reviewers.add( review.author.login )
 		);
@@ -110,7 +125,13 @@ export async function getContributorsList() {
 
 	// Process pull request comments.
 	contributorData.comments.nodes
-		.filter( ( comment ) => ! skipUser( comment.author.login ) )
+		.filter( ( comment ) => {
+			if ( ! comment.author ) {
+				hasGhostActivity = true;
+				return false;
+			}
+			return ! skipUser( comment.author.login );
+		} )
 		.forEach( ( comment ) =>
 			contributors.commenters.add( comment.author.login )
 		);
@@ -120,12 +141,18 @@ export async function getContributorsList() {
 
 	// Process reporters and commenters for linked issues.
 	for ( const linkedIssue of contributorData.closingIssuesReferences.nodes ) {
-		if ( ! skipUser( linkedIssue.author.login ) ) {
+		// Lay a proton trap for any more ghosts.
+		if ( ! linkedIssue.author ) {
+			hasGhostActivity = true;
+		} else if ( ! skipUser( linkedIssue.author.login ) ) {
 			contributors.reporters.add( linkedIssue.author.login );
 		}
 
 		for ( const issueComment of linkedIssue.comments.nodes ) {
-			if ( skipUser( issueComment.author.login ) ) {
+			if (
+				! issueComment.author ||
+				skipUser( issueComment.author.login )
+			) {
 				continue;
 			}
 
@@ -229,6 +256,9 @@ export async function getContributorsList() {
 			} )
 			.filter( ( el ) => el );
 	} );
+
+	// Include findings so Ghostbuster HQ can be notified.
+	contributorLists.hasGhostActivity = hasGhostActivity;
 
 	core.debug( contributorLists );
 
